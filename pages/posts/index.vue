4202659dc901a7905b603b14b9c4a28695add9fe
gze1206 <gze1206@naver.com>
2021-04-17 00:01:54 +0900

    post list query test

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,9 +1,24 @@
 <template>
+  <ul>
+    <li v-for="entity in entries" :key="entity.sys.id">{{ entity.fields.title }}</li>
+  </ul>
 </template>
 
 <script>
+import { createClient } from '~/plugins/contentful.js';
+
+const client = createClient();
+
 export default {
-  components: {
+  async asyncData ({ env, params }) {
+    let res = await client.getEntries({
+      'content_type': env.CTF_BLOG_POST_TYPE_ID,
+      'sys.id[slug]': params.slug
+    });
+
+    return {
+      entries: res.items
+    }
   }
 }
 </script>
